d6fa8c21cf42d73adaee6ecdb2b58239cdb2b5c3
Mathew <matymcleod@gmail.com>
2021-08-18 14:51:36 -0700

    adding to the actual question/answer generator

--- questions.rb
+++ questions.rb
@@ -1,4 +1,10 @@
 class Questions
+  attr_reader :new_question, :answer
+
   def initialize
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    @new_question = "What is the sum of #{num1} plus #{num2} ?"
+    @answer = num1 = num2
   end
 end
